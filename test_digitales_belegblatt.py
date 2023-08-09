8192b0883db0c87abe998a2a99f3688ae019be3f
Dirk Friedenberger <Dirk.Friedenberger@deutschebahn.com>
2023-08-09 11:45:26 +0200

    reduce pylint errors

--- test_digitales_belegblatt.py
+++ test_digitales_belegblatt.py
@@ -1,39 +1,46 @@
 from  datetime import datetime
+from PIL import Image 
+
+from digitales_belegblatt.digitales_belegblatt import DigitalesBelegblatt
+
 class TimerMock():
 
     def __init__(self):
         self._now = datetime.now()
+
     def now(self):
         return self._now
-    
+
+    def set_time(self,time_string : str):
+        self._now = datetime.strptime(time_string, '%m/%d/%y %H:%M:%S')
+
 def test_zug_position():
-    from digitales_belegblatt.digitales_belegblatt import DigitalesBelegblatt
-    from PIL import Image 
+
     timer_mock = TimerMock()
-    db = DigitalesBelegblatt(["Schwarze Pumpe","Boxberg","Cottbus"])
-    db.timer = timer_mock
+    digitales_belegblatt = DigitalesBelegblatt(["Schwarze Pumpe","Boxberg","Cottbus"])
+    digitales_belegblatt.timer = timer_mock
 
-    timer_mock._now = datetime.strptime('09/19/22 13:55:26', '%m/%d/%y %H:%M:%S')
-    db.set_zug_position(101,"Schwarze Pumpe")
-    db.set_zug_position(102,"Cottbus")
+    timer_mock.set_time('09/19/22 13:55:26')
+    digitales_belegblatt.set_zug_position(101,"Schwarze Pumpe")
+    digitales_belegblatt.set_zug_position(102,"Cottbus")
 
-    assert "Schwarze Pumpe" == db.get_zug_position(101)
-    assert "Cottbus" == db.get_zug_position(102)
+    assert "Schwarze Pumpe" == digitales_belegblatt.get_zug_position(101)
+    assert "Cottbus" == digitales_belegblatt.get_zug_position(102)
 
-    db.block_strecke_for_zugnummer(102,"Schwarze Pumpe")
+    digitales_belegblatt.block_strecke_for_zugnummer(102,"Schwarze Pumpe")
 
-    timer_mock._now = datetime.strptime('09/19/22 14:10:26', '%m/%d/%y %H:%M:%S')
-    db.set_zug_position(102,"Boxberg")
-    timer_mock._now = datetime.strptime('09/19/22 14:13:00', '%m/%d/%y %H:%M:%S')
-    db.set_zug_position(102,"Schwarze Pumpe")
+    timer_mock.set_time('09/19/22 14:10:26')
+    digitales_belegblatt.set_zug_position(102,"Boxberg")
+    timer_mock.set_time('09/19/22 14:13:00')
+    digitales_belegblatt.set_zug_position(102,"Schwarze Pumpe")
 
-    timer_mock._now = datetime.strptime('09/19/22 14:20:26', '%m/%d/%y %H:%M:%S')
+    timer_mock.set_time('09/19/22 14:20:26')
 
-    db.block_strecke_for_zugnummer(101,"Boxberg")
-    
-    timer_mock._now = datetime.strptime('09/19/22 14:30:26', '%m/%d/%y %H:%M:%S')
-    db.set_zug_position(101,"Boxberg")
+    digitales_belegblatt.block_strecke_for_zugnummer(101,"Boxberg")
+
+    timer_mock.set_time('09/19/22 14:30:26')
+    digitales_belegblatt.set_zug_position(101,"Boxberg")
 
 
-    img : Image = db.generate_image()
+    img : Image = digitales_belegblatt.generate_image()
     img.save("test.png")
