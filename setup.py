c9fa181ff37412ec9cd5bc84b6b935983ddc69bb
Dirk Friedenberger <Dirk.Friedenberger@deutschebahn.com>
2023-12-31 16:10:30 +0100

    added pipeline

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 from digitales_belegblatt import __version__
 
-setup(name='digitales_belegblatt',
+setup(name='digitales-belegblatt',
       version=__version__,
       description='Implementation of Digitales Belegblatt.',
-      url='git@gitlab.hpi.de:osm/flexidug/digitales-belegblatt.git',
+      long_description=open('README.md', encoding="UTF-8").read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/simulate-digital-rail/Digitales-Belegblatt.git',
       author='Dirk Friedenberger',
       author_email='projekte@frittenburger.de',
       license='GPLv3',
